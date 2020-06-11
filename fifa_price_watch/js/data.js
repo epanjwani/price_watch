86164e0ae26f1811af5a5a2f570da6bd2437c39f
Eashan Panjwani <eashan.panjwani@wustl.edu>
2020-06-11 16:36:03 +0000

    Updated js file to reflect new instance ip address

--- fifa_price_watch/js/data.js
+++ fifa_price_watch/js/data.js
@@ -8,7 +8,7 @@ function getData(type, length)
     timezone_offset = now.getTimezoneOffset()/60;
     let startTime = (new Date(now.getTime() - 1000*60*60*24*length + 1000*60*60*24*length*offset_from_beginning)).toUTCString();
     let increments = (2*24*length).toString();
-    let request_url = ("http://ec2-18-188-83-141.us-east-2.compute.amazonaws.com/~ep/price_watch/fifa_price_watch/php/getdata.php").concat(
+    let request_url = ("http://ec2-18-223-100-110.us-east-2.compute.amazonaws.com/~ep/price_watch/fifa_price_watch/php/getdata.php").concat(
     "?startTime=", startTime, "&increments=", increments, "&table=", type, "&offset=", timezone_offset);
     $.ajax({
         method: "GET",
@@ -37,7 +37,7 @@ function getSidebarData()
     initial.setHours(0);
     initial.setMinutes(0);
     let initial_utc = initial.toUTCString();
-    let request_url = ("http://ec2-18-188-83-141.us-east-2.compute.amazonaws.com/~ep/price_watch/fifa_price_watch/php/getsidebardata.php").concat(
+    let request_url = ("http://ec2-18-223-100-110.us-east-2.compute.amazonaws.com/~ep/price_watch/fifa_price_watch/php/getsidebardata.php").concat(
     "?initial_utc=", initial_utc); 
     $.ajax({
         method: "GET",
